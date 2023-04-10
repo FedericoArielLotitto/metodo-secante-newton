0318f2d1dd25e0b3745add125f2973d555859038
FedericoArielLotitto <federicoariellotitto@gmail.com>
2023-04-10 19:00:02 -0300

    Agrega función estática para aproximar raíz

---
+++ TP1MetodosNumericos.js
@@ -0,0 +1,8 @@
+const functionEquisAlCuboMenosTres = (x) => Math.pow(x, 3) - 3; 
+
+function secanteNewton(limiteIntervaloInferior, limiteIntervaloSuperior, epsilon) {
+    console.log(functionEquisAlCuboMenosTres(3));
+    return ;
+}
+
+secanteNewton();
