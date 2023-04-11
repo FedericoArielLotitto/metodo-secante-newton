f9b1f7c10164c0848d2761a6cb2de51cc2e54cf2
FedericoArielLotitto <federicoariellotitto@gmail.com>
2023-04-10 22:45:05 -0300

    Agrega metodo secante

--- TP1MetodosNumericos.js
+++ TP1MetodosNumericos.js
@@ -2,24 +2,61 @@
     --> Cuando se pasa una cota que no tiene raíz en el medio -> Verificar por teorema del valor intermedio
     --> Se pasa un epsilon negativo
     --> Cuando se pasa al revés el orden de los límtes de la cota (primero el fin, después el principio)
+    --> Cuando la derivada de la función retorne 0
 
 La condición de parada de este ejercicio es el epsilon (#ver de agregar la cantidad de pasos, no recuerdo 
     cómo calcular el orden de convergencia en este caso)
 
 */
 
-const functionEquisAlCuboMenosTres = (x) => Math.pow(x, 3) - 3; 
+const funcionAEvaluar = x => Math.pow(x, 3) - 3; 
+const derivadaPrimeraEquisCuboMenosTres = x => 3*x
+const derivadaSegundaEquisCuboMenosTres = 3;
+
+function newton() {
+    return;
+}
+
+function funcionSecante(funcion, primerValorAnterior, segundoValorAnterior) {
+    let p1 = funcion(primerValorAnterior);
+    let p0 = funcion(segundoValorAnterior);
+    let numerador = p1 * (primerValorAnterior - segundoValorAnterior);
+    let denominador = p1 - p0;
+    return primerValorAnterior - (numerador / denominador);
+}
 
 function errorRelativo(valorAproximado) {
-    let raizEquisAlCuboMenosTres = Math.pow(3, 1/3);
-    return (raizEquisAlCuboMenosTres - valorAproximado) / raizEquisAlCuboMenosTres;
+    const raizEquisAlCuboMenosTres = Math.pow(3, 1/3);
+    return Math.abs((raizEquisAlCuboMenosTres - valorAproximado)) / raizEquisAlCuboMenosTres;
 }
 
-function secanteNewton(limiteIntervaloInferior, limiteIntervaloSuperior, epsilon) {
-    console.log(functionEquisAlCuboMenosTres(3));
-    return ;
+function errorAbsoluto(valorAproximado) {
+    return Math.abs(Math.pow(3, 1/3) - valorAproximado);;
 }
 
-console.log(errorRelativo(1.44));
+function metodoSecante(primerValorAnterior, segundoValorAnterior, limitePasos, epsilon) {
+    let raizAproximada = funcionSecante(funcionAEvaluar, primerValorAnterior, segundoValorAnterior);
+    let cantidadPasos = 1;
+    console.log(cantidadPasos, ": ", raizAproximada);
+    let errorAlcanzado = errorAbsoluto(raizAproximada);
+    console.log("Error primero: ", errorAlcanzado);
 
-secanteNewton();
+    while(cantidadPasos > limitePasos || errorAlcanzado > epsilon) {
+        let nuevoPrimerValorAnterior = raizAproximada;
+        let nuevoSegundoValorAnterior = primerValorAnterior;
+        raizAproximada = funcionSecante(funcionAEvaluar, nuevoPrimerValorAnterior, nuevoSegundoValorAnterior);
+        errorAlcanzado = errorAbsoluto(raizAproximada);
+        console.log("Error alcanzado: ", errorAlcanzado);
+        console.log("Raíz: ", raizAproximada);
+        cantidadPasos += 1;
+    }
+    
+    return  { 
+        raizAproximada: raizAproximada,
+        errorAlcanzado: errorAlcanzado,
+        cantidadPasos: cantidadPasos
+    };
+}
+
+
+metodoSecante(2, 1, 25, 0.0001);
